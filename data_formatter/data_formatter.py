a4318d5c348d121020ea542b0cd7e6ecebc6977e
rahulavasarala <94878030+rahulavasarala@users.noreply.github.com>
2022-03-27 12:41:56 -0700

    updated data_formatter- no more mismatches

--- data_formatter/data_formatter.py
+++ data_formatter/data_formatter.py
@@ -1,7 +1,7 @@
 import shutil, os
 
-setName = "valid" #CHANGE THIS
-annotation_switch_arr = [0,0,1,2,-1,4,5,6,7,8,9,10,11]#CHANGE THIS
+setName = "train" #CHANGE THIS
+annotation_switch_arr = [0,1,2,3,4,5,6,7,8,9,10,11,12]#CHANGE THIS
 
 
 ##------------------Touch with your own Discretion---------------------------------##
@@ -10,24 +10,29 @@ f_folder_name = setName + "F"
 os.mkdir("./{dest}".format(dest = f_folder_name))
 
 jpgCount = 0
-txtCount = 0
 
 for file in os.listdir("./{dest}".format(dest = setName)):
     if file.endswith(".jpg"):
+        print(file)
         shutil.copy("./{dest}/{filename}".format(dest = setName, filename = file), "./{dest}".format(dest = f_folder_name))
         os.rename("./{dest}/{filename}".format(dest = f_folder_name, filename = file), "./{dest}/{name}-{x}.jpg".format(dest = f_folder_name,name = setName, x = jpgCount))
-        jpgCount += 1
 
-    elif file.endswith(".txt"):
-        annotFile = open("./{dest}/{name}-{x}.txt".format(dest = f_folder_name,name = setName, x = txtCount), 'w')
-        #file is jst a string, to read from it we need to open a file
 
-        readableFile = open("./{dest}/{filename}".format(dest = setName, filename = file), 'r')
+        #extract the beginning of the file without the .jpg
+        #append .txt to it 
+        #open the .txt  file in read only mode
+        #read each line and do the switch annotation arr logic on it
+        #write the lines to the new annotation file
+
+        stem = file[0:-4]
+        print(stem)
+        annotFile = open("./{dest}/{name}-{x}.txt".format(dest = f_folder_name,name = setName, x = jpgCount), 'w')
+        readableFile = open("./{dest}/{filename}.txt".format(dest = setName, filename = stem), 'r')
+        print("This runs")
 
         line = readableFile.readline()
 
         while line:
-            
             splitArr = line.split(" ", 1)
 
             if annotation_switch_arr[int(splitArr[0])] == -1:
@@ -37,11 +42,16 @@ for file in os.listdir("./{dest}".format(dest = setName)):
             modLine = modLine + splitArr[1]
             annotFile.write(modLine)
             line = readableFile.readline()
-            
 
+
+
+        jpgCount += 1
+        print(jpgCount)
         annotFile.close()
         readableFile.close()
-        txtCount += 1
 
 ##------------------Touch with your own Discretion---------------------------------##
+
+
+
     
